ed5935c878d9cf16e5144186ff75cc847cbc42a8
JIHYE KO <95533614+kokojihye@users.noreply.github.com>
2024-02-04 18:01:51 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 10.79 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,11 @@
+import java.lang.Character;
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        for(char c : myString.toCharArray()) {
+            answer += Character.toLowerCase(c);
+        }
+        return answer;
+    }
+}
