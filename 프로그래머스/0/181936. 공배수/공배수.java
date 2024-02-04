9d14c2f04fceec71b21335834ead3655102a6b84
JIHYE KO <95533614+kokojihye@users.noreply.github.com>
2024-02-04 18:18:27 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        int a = number % n;
+        int b = number % m;
+        
+        if (a == 0 && b == 0) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        return answer;
+    }
+}
