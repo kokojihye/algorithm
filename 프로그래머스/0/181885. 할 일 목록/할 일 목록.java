1124952ba1b81ba2c01d64a754e2a03ac52fbbfd
JIHYE KO <jihyevivianako@gmail.com>
2024-03-27 15:11:34 +0900

    [level 0] Title: 할 일 목록, Time: 10.11 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,22 +1,9 @@
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        int size = 0;
-        int num = 0;
-        
+        String str = "";
         for(int i = 0; i < finished.length; i++) {
-            if(finished[i] == false) {
-                size++;
-            }
+            str = finished[i] == false ? str+todo_list[i] + "," : str;
         }
-        
-        String[] answer = new String[size];
-        
-        for(int i = 0; i < finished.length; i++) {
-            if(finished[i] == false) {
-                answer[num] = todo_list[i];
-                num++;
-            }
-        }
-        return answer;
+        return str.split(",");
     }
 }
