a416705e6dbcd1b54e146f6c193003e9b95095c3
JIHYE KO <jihyevivianako@gmail.com>
2024-03-29 14:19:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.79 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,20 +3,20 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> hm = new HashMap<>();
-
-        // participant 배열을 순회하며 각 이름의 등장 횟수를 세기
-        Arrays.stream(participant)
-                .forEach(p -> hm.put(p, hm.getOrDefault(p, 0) + 1));
-
-        // completion 배열을 순회하며 해당 이름의 값을 1씩 줄이기
-        Arrays.stream(completion)
-                .forEach(c -> hm.put(c, hm.get(c) - 1));
-
-        // 값이 0보다 큰 이름을 찾아서 반환하기
-        return hm.entrySet().stream()
-                .filter(entry -> entry.getValue() > 0)
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse("");
+        String answer = "";
+        
+        for(String p : participant) {
+            hm.put(p, hm.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+            hm.put(c, hm.get(c) -1);
+        }
+        for(String key : hm.keySet()) {
+            if(hm.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        return answer;
     }
 }
