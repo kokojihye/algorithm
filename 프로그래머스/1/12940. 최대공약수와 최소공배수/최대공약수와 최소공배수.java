c074d96e10f380ca10ecda0caacc00d85187d7b8
JIHYE KO <jihyevivianako@gmail.com>
2024-04-04 15:57:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0] = gcd(n, m);
+        answer[1] = n * m / gcd(n, m);
+        return answer;
+    }
+    
+    public int gcd(int n, int m) {
+        while(m != 0) {
+            int temp = m;
+            m = n % m;
+            n = temp;
+        }
+        return n;
+    }
+}
