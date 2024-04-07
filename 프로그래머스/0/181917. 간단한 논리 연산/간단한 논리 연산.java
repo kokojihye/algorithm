df9e0f2feb442ee6bb9ded28ef1e191bc1c84e76
JIHYE KO <jihyevivianako@gmail.com>
2024-04-07 12:40:01 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-        boolean temp1 = false;
-        boolean temp2 = false;
-        boolean answer = false;
-        if(x1 || x2 == true) {
-            temp1 = true;
-        }
-        if(x3 || x4 == true) {
-            temp2 = true;
-        }
-        if(temp1 && temp2 == true) {
-            answer = true;
-        }
-        return answer;
+        return (x1||x2)&&(x3||x4);
     }
 }
