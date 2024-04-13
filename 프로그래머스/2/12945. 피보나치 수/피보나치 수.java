4e93bde4253ceedee0a69d7a741daf23fc769fd0
JIHYE KO <jihyevivianako@gmail.com>
2024-04-13 14:07:39 +0900

    [level 2] Title: 피보나치 수, Time: 3.19 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -4,10 +4,9 @@ class Solution {
         answer[0] = 0;
         answer[1] = 1;
         
-        for(int i=2; i<=n; i++){
-            answer[i] = (answer[i-1]+answer[i-2])%1234567;
+        for(int i = 2; i <=n; i++) {
+            answer[i] = ((answer[i-1] % 1234567) + (answer[i-2] % 1234567)) %1234567;
         }
-        
-        return answer[n]%1234567;
+        return answer[n] % 1234567;
     }
 }
