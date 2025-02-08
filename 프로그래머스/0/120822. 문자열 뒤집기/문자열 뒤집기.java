84a544e50d5f08a0e9407259b10b6a52bb6317df
JIHYE KO <jihyevivianako@gmail.com>
2025-02-08 17:24:25 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        char[] charMyString = my_string.toCharArray();
-        int size = my_string.length();
-        StringBuilder sb = new StringBuilder();
-        
-        for(int i= size -1; i >= 0; i--) {
-            sb.append(charMyString[i]);
-        }
-        return sb.toString();
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
